d4cb57b3fb5d4717020d7c36f3094ccfd7d8bbd5
Yura <mr.Shapovaloff3101@gmail.com>
2021-10-05 12:02:51 +0300

    1.3. Добавит React-компонент main-page

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import MainPage from '../main-page/main-page';
 
 function App() {
-  return <p>Hello, world!</p>;
+  return (
+    <MainPage />
+  );
 }
 
 export default App;
