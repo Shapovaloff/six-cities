27f56e94786c22cfebc28e660367e49fb04c49a4
Yura <mr.Shapovaloff3101@gmail.com>
2021-10-09 18:01:57 +0300

    5.3. Добавит поддержку инструмента Redux DevTools.

--- project/src/index.js
+++ project/src/index.js
@@ -6,8 +6,9 @@ import reviews from './mocks/reviews';
 import {createStore} from '@reduxjs/toolkit';
 import {reducer} from './store/reducer';
 import {Provider} from 'react-redux';
+import {composeWithDevTools} from 'redux-devtools-extension';
 
-const store = createStore(reducer);
+const store = createStore(reducer, composeWithDevTools());
 
 ReactDOM.render(
   <React.StrictMode>
